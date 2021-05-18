db63dc0e974f67d5575ab6702e033abbcc23e00a
Supun Priyashanka <79795518+supunpriyashanka@users.noreply.github.com>
2021-05-18 16:57:18 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -11,8 +11,8 @@ from Tools.Download import download
 
 
 
-my_father = "https://t.me/{}".format(Config.USER_NAME[1:])
-support = "https://telegram.dog/Ns_Bot_supporters"
+my_father = "https://t.me/NET_HACKER_BOTs_chat".format(Config.USER_NAME[1:])
+support = "https://t.me/Network_hacker_bots"
 
 @Client.on_message(Filters.command(["start"]))
 async def start(c, m):
@@ -20,7 +20,7 @@ async def start(c, m):
     await c.send_message(chat_id=m.chat.id,
                          text=Translation.START.format(m.from_user.first_name, Config.USER_NAME),
                          reply_to_message_id=m.message_id,
-                         reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("My Father 👨‍💻", url=my_father), InlineKeyboardButton("📌Support channel", url=support)]]))
+                         reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("📌Support Group", url=my_father), InlineKeyboardButton("📌Support channel", url=support)]]))
     logger.info(f"{m.from_user.first_name} used start command")
 
 
@@ -99,7 +99,7 @@ async def login(c, m):
 
         if (len(m.command) < 2) & (m.from_user.id not in Config.LOGGED_USER) & (m.from_user.id not in Config.AUTH_USERS):
             await c.send_message(chat_id=m.chat.id,
-                                 text="Use this command for login to this bot. Semd the passwordin the format 👉`/login Bot password`.",
+                                 text="Use this command for login to this bot. Send the password in the format 👉`/login Bot password`.",
                                  disable_web_page_preview=True,
                                  reply_to_message_id=m.message_id,
                                  parse_mode="markdown")
